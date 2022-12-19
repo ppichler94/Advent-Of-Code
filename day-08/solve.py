508a9327087d92aee73777d5893b4d2724934e08
Patrick Pichler <ppichler@outlook.at>
2022-12-19 21:16:52 +0100

    solves part b of day 08 and adds readme

--- day-08/solve.py
+++ day-08/solve.py
@@ -58,6 +58,37 @@ def solve_b(input):
 
 def calculate_scenic_scores(input):
     score = np.zeros(input.shape, dtype=int)
+    for row in range(1, input.shape[0] - 1):
+        for col in range(1, input.shape[1] - 1):
+            score[row, col] = calculate_scenic_score(input, row, col)
+    return score
+
+
+def calculate_scenic_score(input, row, col):
+    up = calculate_scenic_score_col(input, col, row, -1, -1)
+    down = calculate_scenic_score_col(input, col, row, input.shape[0], 1)
+    left = calculate_scenic_score_row(input, row, col, -1, -1)
+    right = calculate_scenic_score_row(input, row, col, input.shape[1], 1)
+    return up * down * left * right
+
+
+def calculate_scenic_score_row(input, row, start, stop, step):
+    score = 0
+    house_height = input[row, start]
+    for col in range(start + step, stop, step):
+        score += 1
+        if input[row, col] >= house_height:
+            return score
+    return score
+
+
+def calculate_scenic_score_col(input, col, start, stop, step):
+    score = 0
+    house_height = input[start, col]
+    for row in range(start + step, stop, step):
+        score += 1
+        if input[row, col] >= house_height:
+            return score
     return score
 
 
