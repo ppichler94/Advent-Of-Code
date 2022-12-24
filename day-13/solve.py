ade2951eac2e508811bd06f1afebe9face728830
Patrick Pichler <ppichler@outlook.at>
2022-12-24 14:51:35 +0100

    removes debug prints

--- day-13/solve.py
+++ day-13/solve.py
@@ -22,11 +22,9 @@ def solve_a(input):
     packet_pairs = parse_input(input)
     result = 0
     for index, [packet1, packet2] in enumerate(packet_pairs):
-        print(f"\n== Pair {index+1} ==")
         comparison_result = compare_lists(packet1, packet2, 0)
         if comparison_result == -1:
             result += (index + 1)
-            print(f"Pair is in right order")
     return result
 
 
@@ -40,15 +38,12 @@ def parse_input(input):
 
 
 def compare_lists(list1, list2, offset):
-    print(f'{" "*offset}Compare {list1} vs {list2}')
     while 1:
         if len(list1) == 0 and len(list2) == 0:
             return 0
         if len(list1) == 0:
-            print(f'{" "*(offset+2)} Left side ran out of items')
             return -1
         if len(list2) == 0:
-            print(f'{" "*(offset+2)} Right side ran out of items')
             return 1
         data1 = list1.pop(0)
         data2 = list2.pop(0)
@@ -58,13 +53,9 @@ def compare_lists(list1, list2, offset):
 
 
 def compare(data1, data2, offset):
-    print(f'{" "*offset}Compare {data1} vs {data2}')
     if type(data1) == list and type(data2) == list:
         return compare_lists(data1, data2, offset + 2)
     if type(data1) == int and type(data2) == int:
-        if sign(data1 - data2) != 0:
-            side = "Left" if sign(data1 - data2) < 0 else "Right"
-            print(f'{" "*(offset+2)} {side} side is smaller')
         return sign(data1 - data2)
     if type(data1) != list:
         data1 = [data1]
