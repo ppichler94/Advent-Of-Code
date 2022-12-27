6721dfdb221389742734324ccae98f41a8b82894
Patrick Pichler <ppichler@outlook.at>
2022-12-27 13:01:45 +0100

    solves part two of day 15

--- day-15/solve.py
+++ day-15/solve.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 from collections import namedtuple
 
@@ -8,8 +9,8 @@ def main():
 
     print(f'Result example A: {solve_a(example_data, 10)}\n')
     print(f'Result puzzle data A: {solve_a(input_data, 2000000)}\n')
-    print(f'Result example B: {solve_b(example_data)}\n')
-    print(f'Result puzzle data B: {solve_b(input_data)}\n')
+    print(f'Result example B: {solve_b(example_data, 20, 20)}\n')
+    print(f'Result puzzle data B: {solve_b(input_data, 4000000, 4000000)}\n')
 
 
 Beacon = namedtuple("Beacon", ["x", "y"])
@@ -59,8 +60,30 @@ def count_covered_positions(coverages):
     return len(covered)
 
 
-def solve_b(input):
-    return
+def solve_b(input, x_max, y_max):
+    sensors = parse_input(input)
+    for y_level in range(y_max):
+        coverages = [sensor.coverage_at_y_level(y_level) for sensor in sensors]
+        coverages = merge_coverages(coverages)
+        for coverage in coverages:
+            if coverage[0] > 0:
+                return y_level + (coverage[0] - 1) * 4000000
+            if coverage[1] < x_max:
+                return y_level + (coverage[1] + 1) * 4000000
+
+
+def merge_coverages(coverages):
+    if len(coverages) == 0 or len(coverages) == 1:
+        return coverages
+    coverages = [c for c in coverages if c]
+    coverages.sort(key=lambda c:c[0])
+    result = [coverages[0]]
+    for interval in coverages[1:]:
+        if interval[0] <= result[-1][1]:
+            result[-1][1] = max(result[-1][1], interval[1])
+        else:
+            result.append(interval)
+    return result
 
 
 if __name__ == "__main__":
