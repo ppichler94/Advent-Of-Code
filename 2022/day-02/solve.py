594e8fd53ff1abbc52c0393f8cd248194f291a74
Patrick Pichler <ppichler@outlook.at>
2023-01-04 19:41:05 +0100

    refactors day 02 and day 03 of 2022 to use the aoc-basic lib

--- 2022/day-02/solve.py
+++ 2022/day-02/solve.py
@@ -1,4 +1,5 @@
 from enum import IntEnum
+from mylib.aoc_basics import Day
 
 
 class Shape(IntEnum):
@@ -13,96 +14,77 @@ class Outcome(IntEnum):
     LOSS = 0
 
 
-def main():
-    example_data = read_input_from_file("example.txt")
-    input_data = read_input_from_file("input.txt")
+class PartA(Day):
+    def parse(self, text, data):
+        data.lines = text.splitlines()
 
-    print(f'Result example A: {solve_a(example_data)}\n')
-    print(f'Result puzzle data A: {solve_a(input_data)}\n')
-    print(f'Result example B: {solve_b(example_data)}\n')
-    print(f'Result puzzle data B: {solve_b(input_data)}\n')
+    def compute(self, data):
+        score = 0
+        for line in data.lines:
+            codes = line.split(" ")
+            opponent_shape = self.get_shape(codes[0])
+            player_shape = self.get_shape(codes[1])
+            score += self.calculate_score(opponent_shape, player_shape)
+        return score
 
+    def get_shape(self, code):
+        if code == "A" or code == "X":
+            return Shape.ROCK
+        if code == "B" or code == "Y":
+            return Shape.PAPER
+        if code == "C" or code == "Z":
+            return Shape.SCISSORS
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    data = input_file.readlines()
-    input_file.close()
-    data = [x.strip() for x in data]
-    return data
+    def calculate_score(self, opponent_shape, player_shape):
+        score = player_shape
+        if self.is_draw(opponent_shape, player_shape):
+            score += Outcome.DRAW
+        elif self.is_win(opponent_shape, player_shape):
+            score += Outcome.WIN
+        return score
 
+    def is_draw(self, opponent_shape, player_shape):
+        return opponent_shape == player_shape
 
-def solve_a(input):
-    score = 0
-    for line in input:
-        codes = line.split(" ")
-        opponent_shape = get_shape(codes[0])
-        player_shape = get_shape(codes[1])
-        score += calculate_score(opponent_shape, player_shape)
-    return score
-
-
-def get_shape(code):
-    if code == "A" or code == "X":
-        return Shape.ROCK
-    if code == "B" or code == "Y":
-        return Shape.PAPER
-    if code == "C" or code == "Z":
-        return Shape.SCISSORS
-
-
-def calculate_score(opponent_shape, player_shape):
-    score = player_shape
-    if is_draw(opponent_shape, player_shape):
-        score += 3
-    elif is_win(opponent_shape, player_shape):
-        score += 6
-    return score
+    def is_win(self, opponent_shape, player_shape):
+        if player_shape == Shape.ROCK and opponent_shape == Shape.SCISSORS:
+            return True
+        if player_shape == Shape.PAPER and opponent_shape == Shape.ROCK:
+            return True
+        if player_shape == Shape.SCISSORS and opponent_shape == Shape.PAPER:
+            return True
+        return False
 
 
-def is_draw(opponent_shape, player_shape):
-    return opponent_shape == player_shape
+class PartB(PartA):
+    def compute(self, data):
+        score = 0
+        for line in data.lines:
+            codes = line.split(" ")
+            opponent_shape = self.get_shape(codes[0])
+            outcome = self.get_outcome(codes[1])
+            player_shape = self.calculate_shape(opponent_shape, outcome)
+            score += (player_shape + outcome)
+        return score
 
 
-def is_win(opponent_shape, player_shape):
-    if player_shape == Shape.ROCK and opponent_shape == Shape.SCISSORS:
-        return True
-    if player_shape == Shape.PAPER and opponent_shape == Shape.ROCK:
-        return True
-    if player_shape == Shape.SCISSORS and opponent_shape == Shape.PAPER:
-        return True
-    return False
+    def get_outcome(self, code):
+        if code == "X":
+            return Outcome.LOSS
+        if code == "Y":
+            return Outcome.DRAW
+        if code == "Z":
+            return Outcome.WIN
+
+    def calculate_shape(self, opponent_shape, outcome):
+        if outcome == Outcome.DRAW:
+            return opponent_shape
+        if opponent_shape == Shape.ROCK:
+            return Shape.PAPER if outcome == Outcome.WIN else Shape.SCISSORS
+        if opponent_shape == Shape.PAPER:
+            return Shape.SCISSORS if outcome == Outcome.WIN else Shape.ROCK
+        if opponent_shape == Shape.SCISSORS:
+            return Shape.ROCK if outcome == Outcome.WIN else Shape.PAPER
 
 
-def solve_b(input):
-    score = 0
-    for line in input:
-        codes = line.split(" ")
-        opponent_shape = get_shape(codes[0])
-        outcome = get_outcome(codes[1])
-        player_shape = calculate_shape(opponent_shape, outcome)
-        score += (player_shape + outcome)
-    return score
-
-
-def get_outcome(code):
-    if code == "X":
-        return Outcome.LOSS
-    if code == "Y":
-        return Outcome.DRAW
-    if code == "Z":
-        return Outcome.WIN
-
-
-def calculate_shape(opponent_shape, outcome):
-    if outcome == Outcome.DRAW:
-        return opponent_shape
-    if opponent_shape == Shape.ROCK:
-        return Shape.PAPER if outcome == Outcome.WIN else Shape.SCISSORS
-    if opponent_shape == Shape.PAPER:
-        return Shape.SCISSORS if outcome == Outcome.WIN else Shape.ROCK
-    if opponent_shape == Shape.SCISSORS:
-        return Shape.ROCK if outcome == Outcome.WIN else Shape.PAPER
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(2, 2022, PartA, PartB)
--- 2022/day-03/solve.py
+++ 2022/day-03/solve.py
@@ -1,51 +1,38 @@
-def main():
-    example_data = read_input_from_file("example.txt")
-    input_data = read_input_from_file("input.txt")
-
-    print(f'Result example A: {solve_a(example_data)}\n')
-    print(f'Result puzzle data A: {solve_a(input_data)}\n')
-    print(f'Result example B: {solve_b(example_data)}\n')
-    print(f'Result puzzle data B: {solve_b(input_data)}\n')
+from mylib.aoc_basics import Day
 
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    data = input_file.readlines()
-    input_file.close()
-    data = [x.strip() for x in data]
-    return data
+class PartA(Day):
+    def compute(self, data):
+        priority_sum = 0
+        for line in data.text.splitlines():
+            size = len(line)
+            compartment_size = int(size / 2)
+            list_a = line[:compartment_size]
+            list_b = line[compartment_size:]
+            common_item = PartA.find_common_item(list_a, list_b)
+            priority_sum += PartA.priority_of(common_item)
+        return priority_sum
+
+    @classmethod
+    def find_common_item(cls, *lists):
+        return set.intersection(*map(set, lists)).pop()
+
+    @classmethod
+    def priority_of(cls, item):
+        if ord('a') <= ord(item) <= ord('z'):
+            return 1 + ord(item) - ord('a')
+        elif ord('A') <= ord(item) <= ord('Z'):
+            return 27 + ord(item) - ord('A')
 
 
-def solve_a(input):
-    priority_sum = 0
-    for line in input:
-        size = len(line)
-        compartment_size = int(size / 2)
-        list_a = line[:compartment_size]
-        list_b = line[compartment_size:]
-        common_item = find_common_item(list_a, list_b)
-        priority_sum += priority_of(common_item)
-    return priority_sum
+class PartB(PartA):
+    def compute(self, data):
+        priority_sum = 0
+        lines = data.text.splitlines()
+        for i in range(0, len(lines), 3):
+            badge = PartA.find_common_item(*lines[i:i+3])
+            priority_sum += PartA.priority_of(badge)
+        return priority_sum
 
 
-def solve_b(input):
-    priority_sum = 0
-    for i in range(0, len(input), 3):
-        badge = find_common_item(*input[i:i+3])
-        priority_sum += priority_of(badge)
-    return priority_sum
-
-
-def find_common_item(*lists):
-    return set.intersection(*map(set, lists)).pop()
-
-
-def priority_of(item):
-    if (ord(item) >= ord('a') and ord(item) <= ord('z')):
-        return 1 + ord(item) - ord('a')
-    elif (ord(item) >= ord('A') and ord(item) <= ord('Z')):
-        return 27 + ord(item) - ord('A')
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(3, 2022, PartA, PartB)
