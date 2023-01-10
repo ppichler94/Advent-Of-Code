d2867d88b535e2ff2019bc4d4db1761fb4b25e06
Patrick Pichler <ppichler@outlook.at>
2023-01-10 18:46:18 +0100

    Refactor challenges of 2021

--- 2021/day-05/day-05.py
+++ 2021/day-05/day-05.py
@@ -1,96 +1,92 @@
 import numpy as np
 
-
-def readInputFromFile(fileName):
-    inputFile = open(fileName, "r")
-    input = inputFile.readlines()
-    input = [x.strip() for x in input]
-    inputFile.close()
-    return input
+from mylib.aoc_basics import Day
 
 
-class Map:
-    def __init__(self, input, considerDiagonals=False):
-        self.considerDiagonals = considerDiagonals
-        self.parseInput(input)
-        self.calculateDimension()
-        self.map = np.zeros([self.height, self.width], dtype=int)
-        self.addLines()
+class Field:
+    def __init__(self, start_coordinates, end_coordinates, consider_diagonals):
+        self.consider_diagonals = consider_diagonals
+        self.start_coordinates = start_coordinates
+        self.end_coordinates = end_coordinates
+        self.width = 1 + max(self.start_coordinates[:, 0].max(), self.end_coordinates[:, 0].max())
+        self.height = 1 + max(self.start_coordinates[:, 1].max(), self.end_coordinates[:, 1].max())
+        self.data = np.zeros([self.height, self.width], dtype=int)
+        self.add_lines()
 
-    def parseInput(self, input):
-        self.startCoordinates = np.empty([len(input), 2], dtype=int)
-        self.endCoordinates = np.empty([len(input), 2], dtype=int)
-        for index, line in enumerate(input):
+    @classmethod
+    def from_string(cls, text, consider_diagonals):
+        start_coordinates = np.empty([len(text), 2], dtype=int)
+        end_coordinates = np.empty([len(text), 2], dtype=int)
+        for index, line in enumerate(text):
             coords = line.split(" -> ")
-            startCoords = coords[0].split(",")
-            endCoords = coords[1].split(",")
-            self.startCoordinates[index, 0] = int(startCoords[0])
-            self.startCoordinates[index, 1] = int(startCoords[1])
-            self.endCoordinates[index, 0] = int(endCoords[0])
-            self.endCoordinates[index, 1] = int(endCoords[1])
+            start_coords = coords[0].split(",")
+            end_coords = coords[1].split(",")
+            start_coordinates[index, 0] = int(start_coords[0])
+            start_coordinates[index, 1] = int(start_coords[1])
+            end_coordinates[index, 0] = int(end_coords[0])
+            end_coordinates[index, 1] = int(end_coords[1])
+        return Field(start_coordinates, end_coordinates, consider_diagonals)
 
-    def calculateDimension(self):
-        self.width = 1 + max(self.startCoordinates[:, 0].max(), self.endCoordinates[:, 0].max())
-        self.height = 1 + max(self.startCoordinates[:, 1].max(), self.endCoordinates[:, 1].max())
-
-    def addLines(self):
-        for index in range(0, self.startCoordinates.shape[0]):
-            start = self.startCoordinates[index]
-            end = self.endCoordinates[index]
+    def add_lines(self):
+        for index in range(0, self.start_coordinates.shape[0]):
+            start = self.start_coordinates[index]
+            end = self.end_coordinates[index]
             if start[1] == end[1]:
-                self.addHorizontalLine(start[1], start[0], end[0])
+                self.add_horizontal_line(start[1], start[0], end[0])
             elif start[0] == end[0]:
-                self.addVerticalLine(start[0], start[1], end[1])
-            elif self.considerDiagonals:
-                self.addDiagonalLine(start, end)
+                self.add_vertical_line(start[0], start[1], end[1])
+            elif self.consider_diagonals:
+                self.add_diagonal_line(start, end)
 
-    def addHorizontalLine(self, y, x1, x2):
+    def add_horizontal_line(self, y, x1, x2):
         if x2 < x1:
             x1, x2 = x2, x1
         for x in range(x1, x2 + 1):
-            self.map[y, x] += 1
+            self.data[y, x] += 1
 
-    def addVerticalLine(self, x, y1, y2):
+    def add_vertical_line(self, x, y1, y2):
         if y2 < y1:
             y1, y2 = y2, y1
         for y in range(y1, y2 + 1):
-            self.map[y, x] += 1
+            self.data[y, x] += 1
 
-    def addDiagonalLine(self, start, end):
+    def add_diagonal_line(self, start, end):
         dir_x = 1 if start[0] < end[0] else -1
         dir_y = 1 if start[1] < end[1] else -1
         x1, x2 = start[0], end[0]
         y1 = start[1]
         while True:
-            self.map[y1, x1] += 1
+            self.data[y1, x1] += 1
             x1 += dir_x
             y1 += dir_y
             if x1 == x2 + dir_x:
                 break
 
-    def getNumberOfOverlaps(self):
-        return self.map[self.map > 1].size
+    def get_number_of_overlaps(self):
+        return self.data[self.data > 1].size
 
 
-def day05A(input):
-    map = Map(input)
-    return map.getNumberOfOverlaps()
+class PartA(Day):
+    def parse(self, text, data):
+        data.data = Field.from_string(text.splitlines(), False)
+
+    def compute(self, data):
+        return data.data.get_number_of_overlaps()
+
+    def example_answer(self):
+        return 5
 
 
-def day05B(input):
-    map = Map(input, True)
-    return map.getNumberOfOverlaps()
+class PartB(Day):
+    def parse(self, text, data):
+        data.data = Field.from_string(text.splitlines(), True)
+
+    def compute(self, data):
+        return data.data.get_number_of_overlaps()
+
+    def example_answer(self):
+        return 12
 
 
-def main():
-    example = readInputFromFile("day-05/example.txt")
-    input = readInputFromFile("day-05/input.txt")
+Day.do_day(5, 2021, PartA, PartB)
 
-    print(f'Result example A: {day05A(example)}\n')
-    print(f'Result puzzle data A: {day05A(input)}\n')
-    print(f'Result example B: {day05B(example)}\n')
-    print(f'Result puzzle data B: {day05B(input)}\n')
-
-
-if __name__ == "__main__":
-    main()
