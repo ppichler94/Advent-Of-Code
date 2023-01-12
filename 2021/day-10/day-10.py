13dd8122260bca0793228952ec95b56310be7e41
Patrick Pichler <ppichler@outlook.at>
2023-01-12 19:29:34 +0100

    Refactor challenges of 2021

--- 2021/day-10/day-10.py
+++ 2021/day-10/day-10.py
@@ -1,81 +1,75 @@
-import numpy as np
+import statistics
+from mylib.aoc_basics import Day
 
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    input = input_file.readlines()
-    input = [x.strip() for x in input]
-    input_file.close()
-    return input
+class PartA(Day):
+    def parse(self, text, data):
+        data.lines = text.splitlines()
 
+    def compute(self, data):
+        return sum(self.get_points_for_line(line) for line in data.lines)
 
-def check_character(character, stack):
-    matching_character = {"(": ")", "[": "]", "{": "}", "<": ">"}
-    if character in ["(", "[", "{", "<"]:
-        stack.append(character)
-    else:
-        last_character = stack.pop()
-        required_character = matching_character[last_character]
-        if character != required_character:
-            return False
-    return True
-
-
-def get_points_for_corrupt_line(line):
-    point_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
-    stack = []
-    for character in line:
-        matching = check_character(character, stack)
-        if not matching:
-            return point_table[character]
-    return 0
-
-
-def get_points_to_finish_line(line):
-    point_table = {"(": 1, "[": 2, "{": 3, "<": 4}
-    stack = []
-    for character in line:
-        check_character(character, stack)
-    points = 0
-    stack.reverse()
-    for c in stack:
-        points = points * 5 + point_table[c]
-    return points
-
-
-def day10a(input):
-    points = 0
-    for line in input:
-        points += get_points_for_corrupt_line(line)
-    return points
-
-
-def day10b(input):
-    points = []
-    index = 0
-    while index < len(input):
-        line = input[index]
-        corrupt = get_points_for_corrupt_line(line) > 0
-        if corrupt:
-            input.pop(index)
+    @staticmethod
+    def check_character(character, stack):
+        matching_character = {"(": ")", "[": "]", "{": "}", "<": ">"}
+        if character in ["(", "[", "{", "<"]:
+            stack.append(character)
         else:
-            index += 1
-    for line in input:
-        points.append(get_points_to_finish_line(line))
-    points = np.array(points)
+            last_character = stack.pop()
+            required_character = matching_character[last_character]
+            if character != required_character:
+                return False
+        return True
 
-    return np.median(points)
+    def get_points_for_line(self, line):
+        point_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
+        stack = []
+        for character in line:
+            matching = self.check_character(character, stack)
+            if not matching:
+                return point_table[character]
+        return 0
+
+    def example_answer(self):
+        return 26397
+
+    def get_example_input(self, puzzle):
+        return """
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+"""
 
 
-def main():
-    example = read_input_from_file("day-10/example.txt")
-    input = read_input_from_file("day-10/input.txt")
+class PartB(PartA):
+    def compute(self, data):
+        lines = filter(self.is_not_corrupt, data.lines)
+        points = [self.get_points_to_finish_line(line) for line in lines]
+        return statistics.median(points)
 
-    print(f'Result example A: {day10a(example)}\n')
-    print(f'Result puzzle data A: {day10a(input)}\n')
-    print(f'Result example B: {day10b(example)}\n')
-    print(f'Result puzzle data B: {day10b(input)}\n')
+    def is_not_corrupt(self, line):
+        return self.get_points_for_line(line) == 0
+
+    def get_points_to_finish_line(self, line):
+        point_table = {"(": 1, "[": 2, "{": 3, "<": 4}
+        stack = []
+        for character in line:
+            self.check_character(character, stack)
+        points = 0
+        stack.reverse()
+        for c in stack:
+            points = points * 5 + point_table[c]
+        return points
+
+    def example_answer(self):
+        return 288957
 
 
-if __name__ == "__main__":
-    main()
+Day.do_day(10, 2021, PartA, PartB)
