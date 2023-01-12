5ed7a09519b6b980117765a521610abff4d22941
Patrick Pichler <ppichler@outlook.at>
2023-01-12 18:29:32 +0100

    Refactor challenges of 2021

--- 2021/day-09/day-09.py
+++ 2021/day-09/day-09.py
@@ -1,95 +1,53 @@
-import numpy as np
+import nographs as nog
+from mylib.aoc_basics import Day
 
 
-def readInputFromFile(fileName):
-    inputFile = open(fileName, "r")
-    input = inputFile.readlines()
-    input = [x.strip() for x in input]
-    inputFile.close()
-    return input
+class PartA(Day):
+    def parse(self, text, data):
+        data.heightmap = nog.Array([[int(x) for x in list(line)] for line in text.splitlines()])
+        data.moves = nog.Position.moves()
+        data.limits = data.heightmap.limits()
+
+    def compute(self, data):
+        minima = self.get_minima(data)
+        return sum(data.heightmap[x] + 1 for x in minima)
+
+    @staticmethod
+    def get_minima(data):
+        minima = []
+        for y in range(data.limits[0][1]):
+            for x in range(data.limits[1][1]):
+                pos = nog.Position.at(y, x)
+                neighbors = [data.heightmap[x] for x in pos.neighbors(data.moves, data.limits)]
+                if all(data.heightmap[pos] < v for v in neighbors):
+                    minima.append(pos)
+        return minima
+
+    def example_answer(self):
+        return 15
 
 
-def getSurroundingValues(heatmap, i, j):
-    left = heatmap[i, j - 1] if j > 0 else 9
-    right = heatmap[i, j + 1] if j < heatmap.shape[1] - 1 else 9
-    up = heatmap[i - 1, j] if i > 0 else 9
-    down = heatmap[i + 1, j] if i < heatmap.shape[0] - 1 else 9
-    return [left, right, up, down]
+class PartB(PartA):
+    def compute(self, data):
+        minima = self.get_minima(data)
+
+        sizes = [self.get_basin_size(data, pos) for pos in minima]
+        sizes.sort()
+
+        return sizes[-1] * sizes[-2] * sizes[-3]
+
+    @staticmethod
+    def get_basin_size(data, position):
+        def next_edges(pos, _):
+            for next_pos in pos.neighbors(data.moves, data.limits):
+                if data.heightmap[pos] < data.heightmap[next_pos] < 9:
+                    yield next_pos
+
+        traversal = nog.TraversalBreadthFirst(next_edges)
+        return len(set(traversal.start_from(position, already_visited=set()))) + 1
+
+    def example_answer(self):
+        return 1134
 
 
-def getMinima(heatmap):
-    minima = []
-    for i in range(0, heatmap.shape[0]):
-        for j in range(0, heatmap.shape[1]):
-            value = heatmap[i, j]
-            [left, right, up, down] = getSurroundingValues(heatmap, i, j)
-            if value < left \
-                    and value < right \
-                    and value < up \
-                    and value < down:
-                minima.append([i, j])
-    return minima
-
-
-def day09A(input):
-    heatmap = np.array([list(line) for line in input], dtype=int)
-    minima = getMinima(heatmap)
-    risk = [heatmap[x[0], x[1]] + 1 for x in minima]
-    return sum(risk)
-
-
-class Basin:
-    def __init__(self, i, j, heatmap):
-        self.i = i
-        self.j = j
-        self.__points = []
-        self.__calculatePoints(heatmap)
-
-    def __calculatePoints(self, heatmap):
-        pointsToCheck = []
-        pointsToCheck.append([self.i, self.j])
-
-        while len(pointsToCheck) > 0:
-            point = pointsToCheck.pop()
-            if heatmap[point[0], point[1]] < 9 and point not in self.__points:
-                self.__points.append(point)
-
-                value = heatmap[point[0], point[1]]
-                [left, right, up, down] = getSurroundingValues(heatmap, point[0], point[1])
-                if point[0] > 0 and value < up:
-                    pointsToCheck.append([point[0] - 1, point[1]])
-                if point[0] < heatmap.shape[0] - 1 and value < down:
-                    pointsToCheck.append([point[0] + 1, point[1]])
-                if point[1] > 0 and value < left:
-                    pointsToCheck.append([point[0], point[1] - 1])
-                if point[1] < heatmap.shape[1] - 1 and value < right:
-                    pointsToCheck.append([point[0], point[1] + 1])
-
-    def size(self):
-        return len(self.__points)
-
-
-def day09B(input):
-    heatmap = np.array([list(line) for line in input], dtype=int)
-    minima = getMinima(heatmap)
-
-    basins = [Basin(value[0], value[1], heatmap) for value in minima]
-
-    sizes = [basin.size() for basin in basins]
-    sizes.sort()
-
-    return sizes[-1] * sizes[-2] * sizes[-3]
-
-
-def main():
-    example = readInputFromFile("day-09/example.txt")
-    input = readInputFromFile("day-09/input.txt")
-
-    print(f'Result example A: {day09A(example)}\n')
-    print(f'Result puzzle data A: {day09A(input)}\n')
-    print(f'Result example B: {day09B(example)}\n')
-    print(f'Result puzzle data B: {day09B(input)}\n')
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(9, 2021, PartA, PartB)
