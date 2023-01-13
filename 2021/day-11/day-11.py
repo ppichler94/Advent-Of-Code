9792f494e12dbbefb1991edf7bf82869ccb99b63
Patrick Pichler <ppichler@outlook.at>
2023-01-13 15:42:26 +0100

    Refactor challenges of 2021

--- 2021/day-11/day-11.py
+++ 2021/day-11/day-11.py
@@ -1,67 +1,49 @@
+import itertools
+import nographs as nog
 import numpy as np
+from mylib.aoc_basics import Day
 
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    input = input_file.readlines()
-    input = [x.strip() for x in input]
-    input_file.close()
-    return input
+class PartA(Day):
+    def parse(self, text, data):
+        data.energy_levels = np.array([[int(x) for x in line] for line in text.splitlines()], dtype=int)
+        data.moves = nog.Position.moves(diagonals=True)
+        data.limits = ((0, data.energy_levels.shape[0]), (0, data.energy_levels.shape[1]))
+
+    def compute(self, data):
+        return sum(self.step(data) for _ in range(100))
+
+    @staticmethod
+    def step(data):
+        flashed = set()
+        data.energy_levels += 1
+        flashing = np.argwhere(data.energy_levels > 9)
+        while len(flashing):
+            flashing = list(filter(lambda x: x not in flashed,
+                                   [nog.Position(x) for x in np.argwhere(data.energy_levels > 9)]))
+            flashed.update(flashing)
+            increasing = list(itertools.chain.from_iterable(nog.Position(x).neighbors(data.moves, data.limits)
+                                                            for x in flashing))
+            for y, x in increasing:
+                data.energy_levels[y, x] += 1
+        for y, x in flashed:
+            data.energy_levels[y, x] = 0
+        return len(flashed)
+
+    def example_answer(self):
+        return 1656
 
 
-def increase_around(energy_levels, position):
-    i_min = max(0, position[0] - 1)
-    i_max = min(energy_levels.shape[1], position[0] + 2)
-    j_min = max(0, position[1] - 1)
-    j_max = min(energy_levels.shape[0], position[1] + 2)
-    energy_levels[i_min:i_max, j_min:j_max] += 1
+class PartB(PartA):
+    def compute(self, data):
+        for steps in itertools.count(1):
+            self.step(data)
+            if np.all(data.energy_levels == 0):
+                break
+        return steps
+
+    def example_answer(self):
+        return 195
 
 
-def do_step(energy_levels):
-    number_of_flashes = 0
-    energy_levels += 1
-    flashed = np.zeros(energy_levels.shape, dtype=bool)
-    flashing = np.logical_and(energy_levels > 9, ~flashed)
-    flashing = list(np.transpose(flashing.nonzero()))
-    while len(flashing) > 0:
-        index = flashing.pop()
-        increase_around(energy_levels, index)
-        flashed[index[0], index[1]] = True
-        number_of_flashes += 1
-        flashing = np.logical_and(energy_levels > 9, ~flashed)
-        flashing = list(np.transpose(flashing.nonzero()))
-    energy_levels[flashed] = 0
-    return number_of_flashes
-
-
-def day11a(input):
-    number_of_flashes = 0
-    energy_levels = np.array([list(line) for line in input], dtype=int)
-    for _ in range(0, 100):
-        number_of_flashes += do_step(energy_levels)
-    return number_of_flashes
-
-
-def day11b(input):
-    energy_levels = np.array([list(line) for line in input], dtype=int)
-    steps = 0
-    while True:
-        do_step(energy_levels)
-        steps += 1
-        if np.all(energy_levels == energy_levels[0]):
-            return steps
-    return steps
-
-
-def main():
-    example = read_input_from_file("day-11/example.txt")
-    input = read_input_from_file("day-11/input.txt")
-
-    print(f'Result example A: {day11a(example)}\n')
-    print(f'Result puzzle data A: {day11a(input)}\n')
-    print(f'Result example B: {day11b(example)}\n')
-    print(f'Result puzzle data B: {day11b(input)}\n')
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(11, 2021, PartA, PartB)
