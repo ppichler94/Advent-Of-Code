799c854e2d7339bf1c44f7bacf9475d15d462e2a
Patrick Pichler <ppichler@outlook.at>
2023-01-14 10:16:09 +0100

    Refactor challenges of 2021

--- 2021/day-14/day-14.py
+++ 2021/day-14/day-14.py
@@ -1,60 +1,44 @@
+import functools
 from collections import Counter
-from functools import lru_cache
+from mylib.aoc_basics import Day
 
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    input = input_file.readlines()
-    input = [x.strip() for x in input]
-    input_file.close()
-    return input
+class PartA(Day):
+    def parse(self, text, data):
+        data.template, rules_text = text.split("\n\n")
+        data.rules = dict(line.split(" -> ") for line in rules_text.splitlines())
+        data.steps = 10
 
+    def compute(self, data):
+        counter = self.run_steps(data)
+        return max(counter.values()) - min(counter.values())
 
-def parse_input(input):
-    template = input[0]
-    rules = {}
-    for line in input[2:]:
-        s = line.split(" -> ")
-        rules[s[0]] = s[1]
-    return [template, rules]
+    @staticmethod
+    def run_steps(data):
+        @functools.cache
+        def count(pair, step):
+            if step == data.steps or pair not in data.rules:
+                return Counter()
 
+            step += 1
+            insertion = data.rules[pair]
+            counter = Counter(insertion)
+            counter.update(count(pair[0] + insertion, step))
+            counter.update(count(insertion + pair[1], step))
+            return counter
 
-def run_steps(template, rules, step_count):
-    @lru_cache(maxsize=None)
-    def count(pair, step):
-        if step == step_count or pair not in rules:
-            return Counter()
-
-        step += 1
-        insertion = rules[pair]
-        counter = Counter(insertion)
-        counter.update(count(pair[0] + insertion, step))
-        counter.update(count(insertion + pair[1], step))
+        counter = Counter(data.template)
+        for left, right in zip(data.template, data.template[1:]):
+            counter.update(count(left + right, 0))
         return counter
 
-    counter = Counter(template)
-    for left, right in zip(template, template[1:]):
-        counter.update(count(left + right, 0))
-    return counter
+
+class PartB(PartA):
+    def part_config(self, data):
+        data.steps = 40
+
+    def example_answer(self):
+        return 2188189693529
 
 
-def run(input, steps):
-    [template, rules] = parse_input(input)
-    counter = run_steps(template, rules, steps)
-    maximum = max(counter.values())
-    minimum = min(counter.values())
-    return maximum - minimum
-
-
-def main():
-    example = read_input_from_file("day-14/example.txt")
-    input = read_input_from_file("day-14/input.txt")
-
-    print(f'Result example A: {run(example, 10)}\n')
-    print(f'Result puzzle data A: {run(input, 10)}\n')
-    print(f'Result example B: {run(example, 40)}\n')
-    print(f'Result puzzle data B: {run(input, 40)}\n')
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(14, 2021, PartA, PartB)
