e565e55853220c9b609b4e23bb6ca69ee6bc604a
Patrick Pichler <ppichler@outlook.at>
2023-01-15 09:10:53 +0100

    Refactor challenges of 2021

--- 2021/day-17/day-17.py
+++ 2021/day-17/day-17.py
@@ -1,92 +1,67 @@
 import math
-import parse
-import enum
 import itertools
+import re
+from mylib.aoc_basics import Day
 
 
-class Result(enum.Enum):
-    Hit = 0
-    Miss = 1
+class PartA(Day):
+    def parse(self, text, data):
+        match = re.match(r".*x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)", text)
+        if not match:
+            raise RuntimeError("Invalid input")
+        data.limits = [(int(match.group(1)),
+                        int(match.group(2))),
+                       (int(match.group(3)),
+                        int(match.group(4)))]
+
+    def compute(self, data):
+        max_vy = int(-data.limits[1][0] - 1)
+        return int(max_vy * (max_vy + 1) / 2)
+
+    def example_answer(self):
+        return 45
 
 
-def read_input_from_file(file_name):
-    input_file = open(file_name, "r")
-    input = input_file.readlines()
-    input = [x.strip() for x in input]
-    input_file.close()
-    return input
+class PartB(PartA):
+    def compute(self, data):
+        min_vy = data.limits[1][0]
+        max_vy = int(-data.limits[1][0] - 1)
+        min_vx = int(.5 * math.sqrt(1 + 8 * data.limits[0][0]))
+        max_vx = int(data.limits[0][1])
+        velocities = list(itertools.product(range(min_vx, max_vx + 1), range(min_vy, max_vy + 1)))
+        hits = [v for v in velocities if self.is_hit(v, data.limits)]
+        return len(hits)
+
+    @staticmethod
+    def sign(x):
+        if x < 0:
+            return -1
+        if x > 0:
+            return 1
+        return 0
+
+    @classmethod
+    def is_hit(cls, velocity, target):
+        vx, vy = velocity
+        x, y = 0, 0
+        (x_min, x_max), (y_min, y_max) = target
+
+        while True:
+            x += vx
+            y += vy
+
+            vx -= cls.sign(vx)
+            vy -= 1
+
+            if x > x_max:
+                return False
+            if y < y_min:
+                return False
+            if x >= x_min and y <= y_max:
+                return True
+
+    def example_answer(self):
+        return 112
 
 
-def sign(x):
-    if x < 0:
-        return -1
-    if x > 0:
-        return 1
-    return 0
-
-
-def calculate_trajectory(velocity, target):
-    vx, vy = velocity
-    x, y = 0, 0
-    x_min, x_max, y_min, y_max = target
-
-    while True:
-        x += vx
-        y += vy
-
-        vx -= sign(vx)
-        vy -= 1
-
-        if x > x_max:
-            return Result.Miss
-        if y < y_min:
-            return Result.Miss
-        if x >= x_min and y <= y_max:
-            return Result.Hit
-
-
-def parse_target(input):
-    result = parse.parse("target area: x={:d}..{:d}, y={:d}..{:d}", input)
-    return list(result.fixed)
-
-
-def part_b(input, title):
-    print(title)
-    target = parse_target(input[0])
-
-    min_vy = target[2]
-    max_vy = int(-target[2] - 1)
-    min_vx = int(.5 * math.sqrt(1 + 8 * target[0]))
-    max_vx = int(target[1])
-    print(f"velocity range x={min_vx}..{max_vx} y={min_vy}..{max_vy}")
-    velocities = list(itertools.product(range(min_vx, max_vx + 1), range(min_vy, max_vy + 1)))
-    hits = []
-    for velocity in velocities:
-        result = calculate_trajectory(velocity, target)
-        if result == Result.Hit:
-            hits.append(velocity)
-
-    print(f"found {len(hits)} initial velocities")
-
-
-def part_a(input, title):
-    print(title)
-    target = parse_target(input[0])
-    max_vy = int(-target[2] - 1)
-    max_y = int(max_vy * (max_vy + 1) / 2)
-    print(f"max y: {max_y}")
-
-
-def main():
-    example = read_input_from_file("day-17/example.txt")
-    input = read_input_from_file("day-17/input.txt")
-
-    part_a(example, f"example: {example[0]}")
-    part_a(input, f"puzzle data: {input[0]}")
-
-    part_b(example, f"example: {example[0]}")
-    part_b(input, f"puzzle data: {input[0]}")
-
-
-if __name__ == "__main__":
-    main()
+Day.do_day(17, 2021, PartA, PartB)
