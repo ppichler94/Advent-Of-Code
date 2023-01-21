c2083fc1497af7530b5ea5a3797cc12d358d8333
Patrick Pichler <ppichler@outlook.at>
2023-01-21 23:05:09 +0100

    refactors part b of 23/2022

--- 2021/day-23/day-23.py
+++ 2021/day-23/day-23.py
@@ -5,7 +5,6 @@ import nographs as nog
 
 class PartA(Day):
     def parse(self, text, data):
-        data.rooms = ()
         data.room_index = {"A": 0, "B": 1, "C": 2, "D": 3}
         data.cost = {"A": 1, "B": 10, "C": 100, "D": 1000}
         data.end_rooms = (("A", "A"), ("B", "B"), ("C", "C"), ("D", "D"))
@@ -25,14 +24,16 @@ class PartA(Day):
                     return
 
             for pod, room_x, room_y in self.iterate_rooms(rooms):
-                if not any(x != pod for x in rooms[room_x] if x):
+                if room_x == data.room_index[pod] and not any(x != pod for x in rooms[room_x] if x):
                     continue
                 if result := self.move_room_to_room(data, hallway, rooms, (room_y, 2 + 2 * room_x)):
-                    next_rooms, next_hallway, cost = result
-                    yield (next_hallway, next_rooms), cost
+                    next_rooms, cost = result
+                    yield (hallway, next_rooms), cost
                     return
 
             for pod, room_x, room_y in self.iterate_rooms(rooms):
+                if room_x == data.room_index[pod] and not any(x != pod for x in rooms[room_x] if x):
+                    continue
                 for target in data.valid_hallway_pos:
                     pos = 2 + 2 * room_x
                     start = (room_y, pos)
@@ -49,7 +50,7 @@ class PartA(Day):
 
     @staticmethod
     def iterate_rooms(rooms):
-        for room_x, room in enumerate(rooms):
+        for room_x, room in list(enumerate(rooms)):
             for room_y, pod in enumerate(room):
                 if pod:
                     yield pod, room_x, room_y + 1
@@ -87,7 +88,7 @@ class PartA(Day):
             next_end_room if i == end_room_id else next_start_room if i == start_room_id else x
             for i, x in enumerate(rooms)
         )
-        return next_rooms, hallway, self.calculate_cost(data.cost[pod], start, (end_y + 1, end[1]))
+        return next_rooms, self.calculate_cost(data.cost[pod], start, (end_y + 1, end[1]))
 
     def move_room_to_hallway(self, data, hallway, rooms, start, end):
         if self.hallway_blocked(hallway, start[1], end[1]):
@@ -108,9 +109,23 @@ class PartA(Day):
     def calculate_cost(cost, start, end):
         return cost * (start[0] + end[0] + abs(start[1] - end[1]))
 
-
     def example_answer(self):
         return 12521
 
 
-Day.do_day(23, 2021, PartA, None)
+class PartB(PartA):
+    def part_config(self, data):
+        data.end_rooms = (("A", "A"), ("B", "B"), ("C", "C"), ("D", "D"))
+        data.end_rooms = (tuple(["A"] * 4), tuple(["B"] * 4), tuple(["C"] * 4), tuple(["D"] * 4))
+        data.start_rooms = (
+            (data.start_rooms[0][0], "D", "D", data.start_rooms[0][1]),
+            (data.start_rooms[1][0], "C", "B", data.start_rooms[1][1]),
+            (data.start_rooms[2][0], "B", "A", data.start_rooms[2][1]),
+            (data.start_rooms[3][0], "A", "C", data.start_rooms[3][1]),
+        )
+
+    def example_answer(self):
+        return 44169
+
+
+Day.do_day(23, 2021, PartA, PartB)
