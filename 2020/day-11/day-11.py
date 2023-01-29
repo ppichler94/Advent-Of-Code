71d59406309074143660d5ecff67e19482a99b80
Patrick Pichler <ppichler@outlook.at>
2023-01-29 12:50:32 +0100

    refactoring

--- 2020/day-11/day-11.py
+++ 2020/day-11/day-11.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from mylib.aoc_basics import Day
 import nographs as nog
 
@@ -12,14 +14,20 @@ class PartA(Day):
         while True:
             empty_seats = data.array.findall("L")
             filled_seats = data.array.findall("#")
-            to_fill = list(filter(lambda seat: self.count_occupied_neighbors(data, seat) == 0, empty_seats))
-            to_empty = list(filter(lambda seat: self.count_occupied_neighbors(data, seat) >= 4, filled_seats))
+            to_fill = list(filter(partial(self.filter_empty, data), empty_seats))
+            to_empty = list(filter(partial(self.filter_filled_seats, data), filled_seats))
             for seat in to_fill:
                 data.array[seat] = "#"
             for seat in to_empty:
                 data.array[seat] = "L"
             if len(to_fill) == 0 and len(to_empty) == 0:
                 return len(filled_seats)
+
+    def filter_empty(self, data, seat):
+        return self.count_occupied_neighbors(data, seat) == 0
+
+    def filter_filled_seats(self, data, seat):
+        return self.count_occupied_neighbors(data, seat) >= 4
 
     @staticmethod
     def count_occupied_neighbors(data, pos):
@@ -30,18 +38,11 @@ class PartA(Day):
 
 
 class PartB(PartA):
-    def compute(self, data):
-        while True:
-            empty_seats = data.array.findall("L")
-            filled_seats = data.array.findall("#")
-            to_fill = list(filter(lambda seat: self.count_visible_occupied_seats(data, seat) == 0, empty_seats))
-            to_empty = list(filter(lambda seat: self.count_visible_occupied_seats(data, seat) >= 5, filled_seats))
-            for seat in to_fill:
-                data.array[seat] = "#"
-            for seat in to_empty:
-                data.array[seat] = "L"
-            if len(to_fill) == 0 and len(to_empty) == 0:
-                return len(filled_seats)
+    def filter_empty(self, data, seat):
+        return self.count_visible_occupied_seats(data, seat) == 0
+
+    def filter_filled_seats(self, data, seat):
+        return self.count_visible_occupied_seats(data, seat) >= 5
 
     def count_visible_occupied_seats(self, data, pos):
         return sum(1 if self.is_occupied_seat_visible(data, pos, direction) else 0 for direction in data.moves)
